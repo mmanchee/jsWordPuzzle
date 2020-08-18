ef65bdea015291ecd39e9d99cafb9abb0cea4a67
Mike Manchee <mikemanchee@gmail.com>
2020-08-18 10:56:49 -0700

    add js function and jquery to the project
    co-authored-by: David McCaslin <davidmccaslin94@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+function wordScramble(sentence) {
+  let sentArray = sentence.split("");
+  const vowels = ["a", "e", "i", "o", "u"];
+  for (let i = 0; i < sentArray.length; i++) {
+    for (let e = 0; e < vowels.length; e++) {
+      if (vowels[e] === sentArray[i]) {
+        sentArray[i] = "-";
+      }
+    }
+  }
+  return sentArray;
+}
+
+$(document).ready(function() {
+  $("#word-puzzle").click(function(event) {
+    const sent1 = $("input#sentence1").val();
+
+    const puzzle = wordScramble(sent1);
+
+    $("#result").text(puzzle);
+
+    event.preventDefault();
+  })
+});
